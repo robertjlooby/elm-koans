5aa3d97f2314f1e05c81e7a05b2003029673dfef
Rob Looby <robertjlooby@gmail.com>
2014-12-19 16:04:16 -0600

    add AboutLogicalOperators

---
+++ AboutLogicalOperators.elm
@@ -0,0 +1,14 @@
+module AboutLogicalOperators where
+
+import ElmTest.Assertion (..)
+import ElmTest.Test as Test
+import TestHelpers (..)
+
+
+testSuite = Test.suite "About Logical Operators" [
+              Test.test "not negates a boolean" (assertEqual False (not xBool)),
+              Test.test "&& is a logical AND" (assert (True && xBool)),
+              Test.test "|| is a logical OR" (assert (False || xBool)),
+              Test.test "xor is a logical XOR" (assert (False `xor` xBool)),
+              Test.test "otherwise is an alias for True" (assertEqual otherwise xBool)
+            ]
--- ElmKoans.elm
+++ ElmKoans.elm
@@ -3,9 +3,11 @@ import ElmTest.Test as Test
 import AboutAsserts
 import AboutLiterals
 import AboutComparisonOperators
+import AboutLogicalOperators
 
 main = runDisplay (Test.suite "The Elm Koans" [
                      AboutAsserts.testSuite,
                      AboutLiterals.testSuite,
-                     AboutComparisonOperators.testSuite
+                     AboutComparisonOperators.testSuite,
+                     AboutLogicalOperators.testSuite
                   ])
