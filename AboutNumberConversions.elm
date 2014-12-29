7d4d318eab1ce692ed9babe3a0d41aa9fb6ef98e
Rob Looby <robertjlooby@gmail.com>
2014-12-29 13:05:14 -0600

    add AboutNumberConversions

---
+++ AboutNumberConversions.elm
@@ -0,0 +1,17 @@
+module AboutNumberConversions where
+
+import ElmTest.Assertion (..)
+import ElmTest.Test as Test
+import TestHelpers (..)
+
+testSuite = Test.suite "About Number Conversions" [
+              Test.test "toFloat converts an int to a float" (assertEqual xNum (toFloat 5)),
+              Test.test "floor converts an float to a int (rounding down)" (assertEqual xNum (floor 3.6)),
+              Test.test "floor converts an float to a int (rounding down)" (assertEqual xNum (floor -3.6)),
+              Test.test "ceiling converts an float to a int (rounding up)" (assertEqual xNum (ceiling 3.6)),
+              Test.test "ceiling converts an float to a int (rounding up)" (assertEqual xNum (ceiling -3.6)),
+              Test.test "round converts an float to a int (rounding to the closest int)" (assertEqual xNum (round 3.6)),
+              Test.test "round converts an float to a int (rounding to the closest int)" (assertEqual xNum (round -3.6)),
+              Test.test "truncate converts an float to a int (rounding towards 0)" (assertEqual xNum (truncate 3.6)),
+              Test.test "truncate converts an float to a int (rounding towards 0)" (assertEqual xNum (truncate -3.6))
+            ]
--- ElmKoans.elm
+++ ElmKoans.elm
@@ -5,11 +5,13 @@ import AboutLiterals
 import AboutComparisonOperators
 import AboutLogicalOperators
 import AboutMathematicalOperators
+import AboutNumberConversions
 
 main = runDisplay (Test.suite "The Elm Koans" [
                      AboutAsserts.testSuite,
                      AboutLiterals.testSuite,
                      AboutComparisonOperators.testSuite,
                      AboutLogicalOperators.testSuite,
-                     AboutMathematicalOperators.testSuite
+                     AboutMathematicalOperators.testSuite,
+                     AboutNumberConversions.testSuite
                   ])
