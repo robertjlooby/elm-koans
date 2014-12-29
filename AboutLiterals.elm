8d451e5afe006feffb527436b4be2dad8eaede93
Rob Looby <robertjlooby@gmail.com>
2014-12-29 13:41:52 -0600

    add comments to first set of assertions

--- AboutLiterals.elm
+++ AboutLiterals.elm
@@ -5,6 +5,10 @@ import ElmTest.Test as Test
 import TestHelpers (..)
 
 testSuite = Test.suite "About Literals" [
+              -- Single line comments begin with "--"
+              {- Multi line comments
+                 begin with "{-" and end with "-}"
+              -}
               Test.test "strings are enclosed in double quotes" (assertEqual "A string" xString),
               Test.test "characters are enclosed in single quotes" (assertEqual 'A' xChar),
               Test.test "floats have a decimal" (assertEqual 42.24 xNum),
