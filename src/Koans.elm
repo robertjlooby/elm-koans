184d82814e6a51107f5b5537272d41a2f15035c0
Rob Looby <robertjlooby@gmail.com>
2015-07-30 22:30:38 -0500

    add AboutUnionTypes

--- src/Koans.elm
+++ src/Koans.elm
@@ -14,6 +14,7 @@ import AboutResults
 import AboutSets
 import AboutStrings
 import AboutTime
+import AboutUnionTypes
 import ElmTest.Runner.Element exposing (..)
 import ElmTest.Test as Test
 
@@ -38,4 +39,5 @@ main =
          , AboutDates.testSuite
          , AboutSets.testSuite
          , AboutRecords.testSuite
+         , AboutUnionTypes.testSuite
          ]
---
+++ src/Koans/AboutUnionTypes.elm
@@ -0,0 +1,38 @@
+module AboutUnionTypes where
+
+import ElmTest.Assertion exposing (..)
+import ElmTest.Test exposing (suite, test)
+import TestHelpers exposing (..)
+
+
+type Nucleotide
+  = A
+  | C
+  | G
+  | T
+
+
+xNucleotide = G
+
+
+type DNA
+  = Base Nucleotide
+  | Strand (List Nucleotide)
+
+
+testSuite =
+  suite
+    "About UnionTypes"
+    [ test "simple types are similar to enums in other languages"
+        (assertEqual xNucleotide C)
+    , test "more complex types can be built with a 'tag' and additional data"
+        (assertEqual (Base xNucleotide) (Base C))
+    , test "all types in the union type are the same type"
+        (assertEqual (Base A) (Strand [A, T, C, G]))
+    , test "case statements may be used to extract the data from the type"
+        (case Base A of
+          Strand nucleotides ->
+            (assertEqual [xNucleotide] nucleotides)
+          Base nucleotide ->
+            (assertEqual xNucleotide nucleotide))
+    ]
